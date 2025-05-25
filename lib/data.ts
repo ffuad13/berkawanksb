094bfff4f7b9c55571d404c3d1a3411b93b03bba
ffuad13 <jhonatom99@gmail.com>
2025-05-25 23:37:11 +0800

    fix: remove return statement and add getAll query

--- lib/data.ts
+++ lib/data.ts
@@ -15,7 +15,7 @@ export async function insertLaporan(data: LaporanInput) {
   } = data;
 
   try {
-    const result = await sql`
+    await sql`
       INSERT INTO laporan (
         user_id,
 				pelapor,
@@ -36,11 +36,8 @@ export async function insertLaporan(data: LaporanInput) {
         ${bentuk_kegiatan},
         ${tanggal},
         ${waktu}
-      )
-      RETURNING *;
+      );
     `;
-
-    return result[0];
   } catch (error) {
     console.error("Failed to insert laporan:", error);
     throw error;
@@ -56,6 +53,39 @@ export async function getAllLaporan() {
     return results;
   } catch (error) {
     console.error("Error fetching laporan:", error);
+    throw error;
+  }
+}
+
+export async function updateLaporan(data: LaporanInput & { id: string }) {
+  const {
+    id,
+    pelapor,
+    perihal,
+    tempat,
+    pelaksana,
+    sasaran,
+    bentuk_kegiatan,
+    tanggal,
+    waktu,
+  } = data;
+
+  try {
+    await sql`
+      UPDATE laporan
+      SET
+        pelapor = ${pelapor},
+        perihal = ${perihal},
+        tempat = ${tempat},
+        pelaksana = ${pelaksana},
+        sasaran = ${sasaran},
+        bentuk_kegiatan = ${bentuk_kegiatan},
+        tanggal = ${tanggal},
+        waktu = ${waktu}
+      WHERE id = ${id};
+    `;
+  } catch (error) {
+    console.error("Failed to update laporan:", error);
     throw error;
   }
 }
